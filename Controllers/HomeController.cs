04c09437f6d91bf5b0acfbf2a04c2e57c5077e4c
NelsonAlmendares <Nelsonjosealmendares1999@gmail.com>
2024-07-19 17:24:32 -0600

    Detectar MAC desde .Net

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MAC_ADDRESS.Models;
+using MAC_ADDRESS.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Management;
+
+namespace MAC_ADDRESS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MacAddressService _macAddressService;
+
+        public HomeController(MacAddressService macAddressService)
+        {
+            _macAddressService = macAddressService;
+        }
+
+        public IActionResult Index()
+        {
+            var macAddress = _macAddressService.GetMacAddress();
+            ViewBag.MacAddress = macAddress;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Services/MacAddressService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.NetworkInformation;
+
+namespace MAC_ADDRESS.Services
+{
+    public class MacAddressService
+    {
+        public string GetMacAddress()
+        {
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var networkInterface in networkInterfaces)
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    var address = networkInterface.GetPhysicalAddress();
+                    if (address != null && address.ToString() != "")
+                    {
+                        return address.ToString();
+                    }
+                }
+            }
+            return "MAC Address not found";
+        }
+    }
+}
